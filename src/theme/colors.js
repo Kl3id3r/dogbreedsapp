60956165df03ee5938466b93fee4f88c309aa026
Kleider Gonzalez <kleiderdavid19@gmail.com>
2022-05-15 15:51:04 -0400

    [ADD] Theme pallete colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,9 +1,13 @@
 /**
  * @title Colors palette app
  */
-
 const colors = {
     white: '#FAFAFA',
+    whiteSecondary: '#EDEEEE',
+    gray: '#848484',
+    graySecondary: '#757575',
+    green: '#4F8881',
+    yellow: '#CCCE5D',
 }
 
 export default colors;
